68010e437f1ae256e613647947e8f8115546bd5b
Aleksandar Borisavljevic <borisavljevic.aleksandar@outlook.com>
2023-10-22 21:20:55 +0200

    Changes Made: 1:09:00

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -43,11 +43,17 @@ namespace MagicVilla_VillaAPI.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO) 
         {
-            //if (!ModelState.IsValid) 
+            //if (!ModelState.IsValid)
             //{
             //    return BadRequest(ModelState);
             //}
-            if(villaDTO == null) 
+            if(VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower()==villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa already exists!");
+                return BadRequest(ModelState);
+            }
+
+            if (villaDTO == null) 
             {
                 return BadRequest(villaDTO);
             }
